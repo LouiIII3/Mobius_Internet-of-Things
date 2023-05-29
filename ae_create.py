1ba2b252ee56987e83670c7327e681ae48d75c24
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-05-29 16:28:04 +0900

    ae_create.py

---
+++ ae_create.py
@@ -0,0 +1,43 @@
+import json
+import requests
+from requests.exceptions import HTTPError
+import shortuuid
+
+serverIP = "ip주소"
+
+ae = "이름"
+api = "0.0.1"
+
+data = {
+    "m2m:ae": {
+        "rn":  ae,
+        "api": api,
+        "rr": False
+    }
+}
+
+if __name__ == "__main__":
+    headers = {
+        "Accept": "application/json",
+        "X-M2M-RI": "req" + shortuuid.ShortUUID().random(length=10),
+        "X-M2M-Origin": "/S" + ae,
+        "Content-Type": "application/json;ty=2"
+    }
+
+    try:
+        response = requests.request("post",
+                                    "http://" + serverIP + ":7579/Mobius?rcn=1",
+                                    data=json.dumps(data),
+                                    headers=headers,
+                                    verify=False)
+
+        print(response.text)
+
+    except HTTPError as error:
+        print(f"HTTP error occurred: {error}")
+    except Exception as error:
+        print(f"Other error occurred: {error}")
+        
+    except HTTPError as error:
+        print(f"Http error occurred: {error}")
+     
